e85c413c1d01959baf3180a9ce739130bebaaa65
Nicodragon25 <nicolasariellaure@gmail.com>
2024-06-11 07:04:36 -0300

    Add collision check method

--- src/CollisionsHandler.cpp
+++ src/CollisionsHandler.cpp
@@ -3,3 +3,12 @@
 CollisionsHandler::CollisionsHandler()
 {
 }
+
+void CollisionsHandler::CheckCollisions(Entity* entityA, Entity* entityB)
+{
+	if (entityA->GetCollider().intersects(entityB->GetCollider()))
+	{
+		entityA->OnCollisionEnter(entityB);
+		entityB->OnCollisionEnter(entityA);
+	}
+}
